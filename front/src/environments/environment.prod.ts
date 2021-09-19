684453b9c0f1ef7e8fdfbcaf51d293fd17e91b4e
ludo046 <ludomoissinac@icloud.com>
2021-09-19 15:40:29 +0200

    appel api

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  userUrl: 'http://185.216.25.196/:8080/api/users/',
-  ressourceUrl: 'http://185.216.25.196/:8080/api/ressources/',
-  chatUrl: 'http://185.216.25.196/:8080/api/chat'
+  userUrl: 'http://travailaveclesourire.fr/api/users/',
+  ressourceUrl: 'http://travailaveclesourire.fr/api/ressources/',
+  chatUrl: 'http://travailaveclesourire.fr/api/chat/'
 };
 
